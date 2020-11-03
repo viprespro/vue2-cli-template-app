2ed54850cbd4405b4f7fb01c57f8ce38eaa770d9
Aspire01 <18253137881@163.com>
2020-11-03 18:44:03 +0800

    add file module code

--- src/views/files/upload.vue
+++ src/views/files/upload.vue
@@ -31,22 +31,32 @@ export default {
       file.status = "uploading";
       file.message = "上传中...";
       // 此时可以自行将文件上传至服务器
+      // file对象
+      // 1. 单个文件 file是object类型
+      // 2. 多个文件 file是Array类型
       console.log(file);
-      this.doUpload(file);
-
-      setTimeout(() => {
-        file.status = "failed";
-        file.message = "上传失败";
-      }, 1000);
+      if (file instanceof Array) {
+        for (const item of file) {
+          this.doUpload(item);
+        }
+      } else {
+        this.doUpload(file);
+      }
     },
     doUpload(_file) {
+      /**
+       * 说明：后端只是提供一个上传的接口 所以多文件的时候选择循环上传
+       * 上传的每个文件会返回一个resourceKey作为表单提交的时候传递给后端
+       */
       const formData = new FormData();
       formData.append("file", _file.file);
       // 将formData作为参数传递到服务器即可
       // done.then(res => {
       //   file.status = "done"
+      //   file.resourceKey = res.resourceKey  /
       // }).catch(res => {
-
+      //   file.status = "failed"
+      //   ile.message = "上传失败"
       // })
     },
     onOversize() {
